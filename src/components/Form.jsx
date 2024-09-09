20de62824912ddc1d71424c58ae244759f0792bf
unknown <riyas.rio18@gmail.com>
2024-09-09 17:46:25 +0530

    Created server

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,101 @@
-import React from 'react'
+import  React, {useState} from 'react'
 import "../components/styles/styles.css";
+import Swal from 'sweetalert2';
 
 const Form = () => {
+  //useState
+  const [formdata, setFormdata] = useState({
+    name:"",
+    email:"",
+    phone:"",
+    subject:"",
+    message:""
+  });
+
+  const handleChange = (event) =>{
+    const {name, value} = event.target;
+    setFormdata({...formdata, [name]: value,});
+  };
+
+  const handleSubmit = async (event) =>{
+    event.preventDefault();
+
+    try {
+      
+    const response = await fetch("http://127.0.0.1:5000/sendemail", {
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+      },
+      body: JSON.stringify(formdata),
+    });
+
+    if(!response.ok){
+      throw new Error("Network response Error");
+    }
+
+    const result = await response.json();
+    console.log("Form submitted sucessfully", result);
+
+    setFormdata({
+      name:"",
+      email:"",
+      mobile:"",
+      subject:"",
+      message:"",
+    });
+
+    Swal.fire({
+      title: "Good job!",
+      text: "Form Submitted Sucessfully",
+      icon: "success"
+    });
+
+    
+    } catch (error) {
+      console.error("Error submitting data", error);
+    }
+
+  };
+
+
   return (
-    <div className='form'>
+    <div className='form' onSubmit={handleSubmit}>
       <form action="">
         <label>Your Name:</label>
-        <input type="text" />
+        <input type="text" 
+        name="name"
+        value={formdata.name}
+        onChange={handleChange}
+        required />
+
         <label>Email:</label>
-        <input type="text" />
+        <input type="email" 
+        name="email"
+        value={formdata.email}
+        onChange={handleChange}
+        required />
+
+        <label>Mobile:</label>
+        <input type="text" 
+        name="mobile"
+        value={formdata.mobile}
+        onChange={handleChange}
+        maxLength="10"
+        required/>
+
         <label>Subject:</label>
-        <input type="text" />
+        <input type="text" 
+        name="subject"
+        value={formdata.subject}
+        onChange={handleChange}
+        required/>
         <label>Message:</label>
-        <textarea rows="5" placeholder='Type your message here...' />
+        <textarea rows="3" placeholder='Type your message here...' 
+        name="message"
+        value={formdata.message}
+        onChange={handleChange}
+        required/>
         <button className='btn'>Submit </button>
       </form>
     </div>
