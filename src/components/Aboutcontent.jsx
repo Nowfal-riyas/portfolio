e2cf2df34f718ece2db59702e3d6ce522fca4a22
unknown <riyas.rio18@gmail.com>
2024-09-11 10:20:13 +0530

    change: aboutcontent.jsx

--- src/components/Aboutcontent.jsx
+++ src/components/Aboutcontent.jsx
@@ -9,13 +9,13 @@ const Aboutcontent = () => {
     <div className='about'>
       <div className='about_left'>
         <h1>Who Am I ?</h1>
-        <p>I'm a Full stack Application developer.</p>
-        <p>Programming Languages: Python, JavaScript, Dart.</p>
-        <p>Front end: HTML, CSS, Bootstrap.</p>
-        <p>Mobile App: Flutter, Firebase.</p>
-        <p>Back end: Flask, Rest Api, Postman.</p>
-        <p>Version control: Git, Github.</p>
-        <p>Database: MySQL, MongoDb.,</p>
+        <p>I'm a Full stack Application developer. <br/><br/>
+        Programming Languages: Python, JavaScript, Dart.<br/><br/>
+        Front end: HTML, CSS, Bootstrap.<br/><br/>
+        Mobile App: Flutter, Firebase.<br/><br/>
+        Back end: Flask, Rest Api, Postman.<br/><br/>
+        Version control: Git, Github.<br/><br/> 
+        Database: MySQL, MongoDb.</p>
         <Link target="_blank" to="https://drive.google.com/folderview?id=16BE9ryV9JFFjT__NjGFaR1gamTgKPAn5" className='btn'>Resume</Link>
       </div>
       <div className='about_right'>
